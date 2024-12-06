d6e0963bff0b4eb5bd2195b4e8a55c8e9f566a8a
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-06 12:50:42 +0530

    Complete the structure of Contact jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Quotes from "./components/Quotes/Quotes";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <>
-    <Navbar />
-      <Quotes />
+    <Contact />
     </>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,28 @@
 import "./Contact.css";
 
 function Contact() {
-    return ( <>
-    </> );
+  return (
+    <>
+      <div className="contact-container">
+        <div className="contact">
+          <div className="contact-details">
+            <h3>Get in Touch</h3>
+            <p>
+              I'd love to hear from you, whether you just want to shoot the
+              breeze or if you're interested in my work and want to discuss a
+              potential opportunity to achieve great things together!
+            </p>
+          </div>
+          <div className="contact-inputs">
+            <input type="text" name="name" placeholder="Your name" />
+            <input type="text" name="email" placeholder="Your email" />
+            <textarea name="" cols="30" rows="10"></textarea>
+            <button>SEND</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
