b16d0371ee1a1b1dc56afab9fd618463b3ae03d1
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-08 13:32:46 +0530

    Completion of 40 percent of styling for footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
-import Quotes from "./components/Quotes/Quotes";
-import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
     <Navbar />
-    <Quotes />
-    <Contact />
+    <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,56 +6,67 @@ function Footer() {
       <div className="footer-container">
         <div className="footer">
           <div className="footer-details">
-            <h3></h3>
-            <p className="footer-about"></p>
-            <p className="copyright"></p>
+            <h3>GOKUL <br />
+            KRISHNA</h3>
+            <p className="footer-about">
+              A cybersecurity expert from India, committed to identifying web
+              vulnerabilities and enhancing digital security. Driven by a
+              passion for innovative solutions that protect critical systems and
+              ensure data integrity.
+            </p>
+            <p className="copyright">J N Gokul Krishna - Copyright © 2024</p>
           </div>
           <div className="footer-navigation">
-            <h4></h4>
+            <h4>Sitemap</h4>
             <ul className="foo-links">
               <a href="" className="foo-link">
-                <li></li>
+                <li>Home</li>
               </a>
               <a href="" className="foo-link">
-                <li></li>
+                <li>Experience</li>
               </a>
               <a href="" className="foo-link">
-                <li></li>
+                <li>About</li>
               </a>
             </ul>
           </div>
           <div className="footer-links">
-            <h3></h3>
+            <h3>FOLLOW ME ON</h3>
             <ul className="foo-social">
               <a href="">
                 <li>
-                  <img src="" alt="" />
+                <i className="fa-brands fa-linkedin"></i>
                 </li>
               </a>
               <a href="">
                 <li>
-                  <img src="" alt="" />
+                <i className="fa-brands fa-instagram"></i>
                 </li>
               </a>
               <a href="">
                 <li>
-                  <img src="" alt="" />
+                <i className="fa-brands fa-x-twitter"></i>
                 </li>
               </a>
               <a href="">
                 <li>
-                  <img src="" alt="" />
+                <i className="fa-brands fa-github"></i>
                 </li>
               </a>
               <a href="">
                 <li>
-                  <img src="" alt="" />
+                <i className="fa-brands fa-upwork"></i>
                 </li>
               </a>
             </ul>
+            <a href="">
+                <button>
+                    Contact me
+                </button>
+            </a>
           </div>
         </div>
-        <p>
+        <p className="developer-signature">
           Made with ❤️ by <a href="">Adesh D</a>
         </p>
       </div>
