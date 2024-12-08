aefddb0e49792c56a710cdf3a244ff723b4c55a3
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-08 12:54:04 +0530

    Completion of footer component structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,66 @@
-import "./Footer.css"
+import "./Footer.css";
 
 function Footer() {
-    return ( <>
-    
-    </> );
+  return (
+    <>
+      <div className="footer-container">
+        <div className="footer">
+          <div className="footer-details">
+            <h3></h3>
+            <p className="footer-about"></p>
+            <p className="copyright"></p>
+          </div>
+          <div className="footer-navigation">
+            <h4></h4>
+            <ul className="foo-links">
+              <a href="" className="foo-link">
+                <li></li>
+              </a>
+              <a href="" className="foo-link">
+                <li></li>
+              </a>
+              <a href="" className="foo-link">
+                <li></li>
+              </a>
+            </ul>
+          </div>
+          <div className="footer-links">
+            <h3></h3>
+            <ul className="foo-social">
+              <a href="">
+                <li>
+                  <img src="" alt="" />
+                </li>
+              </a>
+              <a href="">
+                <li>
+                  <img src="" alt="" />
+                </li>
+              </a>
+              <a href="">
+                <li>
+                  <img src="" alt="" />
+                </li>
+              </a>
+              <a href="">
+                <li>
+                  <img src="" alt="" />
+                </li>
+              </a>
+              <a href="">
+                <li>
+                  <img src="" alt="" />
+                </li>
+              </a>
+            </ul>
+          </div>
+        </div>
+        <p>
+          Made with ❤️ by <a href="">Adesh D</a>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
