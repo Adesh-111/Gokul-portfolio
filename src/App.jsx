ec310a2e89cfd6aff9603f8e6df0bc78d44a8580
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-09 19:33:46 +0530

    Little changes in Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Quotes from "./components/Quotes/Quotes"
 
 function App() {
   return (
     <>
     <Navbar />
+    
     <Footer />
     </>
   );
