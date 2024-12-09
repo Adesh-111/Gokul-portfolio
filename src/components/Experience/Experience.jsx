2f80dc507dc37370f21e6a9a56f923070e92f22d
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-09 20:07:53 +0530

    Completion of Experience structure

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,47 @@
+import assets from "../../assets/assets";
 import "./Experience.css";
 
-const experiences = [{
-    img : 
-}]
+const experiences = [
+  {
+    img: assets.exImg1,
+    name: "Red Teaming",
+    domain: "WEB-PENTESTER",
+    description:
+      "As a web pentester in cybersecurity, I identify vulnerabilities in web applications to strengthen security. Using tools like Burp Suite, I conduct thorough assessments and deliver detailed reports with actionable insights to safeguard digital assets.",
+  },
+  {
+    img: assets.exImg2,
+    name: "Freelancing",
+    domain: "CYBER SECURITY",
+    description:
+      "I am a cybersecurity specialist focused on web application vulnerabilities and penetration testing. I help businesses strengthen defenses and am seeking freelance opportunities to enhance web security and protect against threats.",
+  },
+];
 
 function Experience() {
-    return ( <>
-
-    </> );
+  return <>
+  <div className="experience-container">
+    <div className="experience">
+        <div className="experience-title">
+            <h3>
+                RECENT EXPERIENCE
+            </h3>
+        </div>
+        {experiences.map((experience, index) => (
+            <div className="experience-card" key={index}>
+                <div className="exp-img">
+                    <img src={experience.img} alt="Exp-img" />
+                </div>
+                <div className="exp-desc">
+                    <h2>{experience.name}</h2>
+                    <h4>{experience.domain}</h4>
+                    <p>{experience.description}</p>
+                </div>
+            </div>
+        ))}
+    </div>
+  </div>
+  </>;
 }
 
 export default Experience;
