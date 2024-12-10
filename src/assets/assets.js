a45bf4351f55541bc375ab8c09097c723b5459f8
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-10 11:54:53 +0530

    Insertion of images for skills

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,18 @@ import exImg2 from "./Ex-img2.png";
 import bottomBack from "./Bottom-bg.png";
 import cybersecIcon from "./Cybersec-icon.svg";
 import cybersecBg from "./Cybersec-icon.svg";
-import GokulKrishna from "./GokulKrishna.jpeg"
+import GokulKrishna from "./GokulKrishna.jpeg";
+import blender from "./blender.svg";
+import figma from "./figma.svg";
+import html from "./html.svg";
+import java from "./java.svg";
+import mysql from "./mySQL.svg";
+import photoShop from "./photoshop.svg";
+import premierPro from "./premier-pro.svg";
+import python from "./python.svg";
+import unity from "./unity.svg";
+import unreal from "./unrealEngine.svg";
+
 
 const assets = {
     Logo,
@@ -15,7 +26,17 @@ const assets = {
     bottomBack,
     cybersecIcon,
     cybersecBg,
-    GokulKrishna
+    GokulKrishna,
+    blender,
+    figma,
+    html,
+    java,
+    mysql,
+    photoShop,
+    premierPro,
+    python,
+    unity,
+    unreal
 };
 
 export default assets;
