d4a8679d4fa62e002bcb51207cee41feda1e04f9
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-11 11:54:18 +0530

    Completion of 35% styling for About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,26 +5,29 @@ function About() {
   return (
     <>
       <div className="about-container">
+        <div className="about">
+
+
         <div className="about-title">
           <h3>ABOUT ME</h3>
         </div>
         <div className="about-details">
           <img src={assets.GokulKrishna} alt="" className="about-img" />
           <p className="about-desc">
-            Born and raised in Aruppukottai, I am currently pursuing an M.Sc. in
+            Born and raised in <span>Aruppukottai</span>, I am currently pursuing an M.Sc. in
             Cyber Forensics and Information Security at the University of
-            Madras. I hold a Bachelor's degree in B.Sc. Information Security and
-            Digital Forensics in Karunya University, which laid the foundation
-            for my career in cybersecurity. <br />
-            With 6 months of experience as a Red Team Analyst at Cybersec in
-            Gurugram, I specialize in web security and penetration testing. I
-            excel in bug hunting, vulnerability scanning, and security
-            documentation, leveraging my skills to strengthen corporate
-            security. I am actively engaged in bug bounty programs to further
-            enhance my expertise in identifying and mitigating security
-            vulnerabilities. <br />
-            Outside of my professional pursuits, I am passionate about
-            slow-paced and art films, immersing myself in their unique
+            Madras. I hold a <span>Bachelor's degree in B.Sc. Information Security and
+            Digital Forensics</span> in Karunya University, which laid the foundation
+            for my career in cybersecurity. <br /> <br /> 
+            With 6 months of experience as a Red Team Analyst at <span>Cybersec</span> in
+            <span> Gurugram</span>, I specialize in web security and penetration testing. I
+            excel in <span>bug hunting</span>, vulnerability scanning, and security
+            documentation, leveraging my skills to strengthen <span>corporate
+            security</span>. I am actively engaged in <span>bug bounty</span> programs to further
+            enhance my expertise in identifying and mitigating <span>security
+            vulnerabilities</span>. <br /> <br />
+            Outside of my professional pursuits, I am passionate about 
+            <span> slow-paced and art films</span>, immersing myself in their unique
             storytelling and artistic expression.
           </p>
         </div>
@@ -102,7 +105,7 @@ function About() {
                   <h4>Attacks</h4>
                   <p>
                     <img src={assets.listStyle} alt="" />
-                    Conducted phishing and spoofing attacks to evaluate clients'
+                    Conducted phishing and spoofing attacks to evaluate clients
                     defenses against social engineering threats. Analyzed
                     results to identify vulnerabilities in employee awareness
                     and response protocols. Provided recommendations to enhance
@@ -161,6 +164,7 @@ function About() {
           </a>
         </div>
         <img src={assets.Arnold} alt="" />
+        </div>
       </div>
     </>
   );
