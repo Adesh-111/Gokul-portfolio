9d60666ac2b8e19a7d33f7beef0d3ec7773dea6c
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-12 11:46:46 +0530

    Add responsiveness for Navbar component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,13 +10,17 @@ function Hero() {
             <h1>GOKUL KRISHNA</h1>
           </div>
           <div className="hero-details">
-            <h4>SCROLL
-            <i className="fa-solid fa-arrow-down"></i>
+            <h4>
+              S<br />
+              C<br />
+              R<br />
+              O<br />
+              L<br />L<i className="fa-solid fa-arrow-down"></i>
             </h4>
             <div className="hero-image">
-              <img src={assets.profilePic} alt="" />
+              <img src={assets.profilePic} alt="" className="profile" />
               <div className="hero-about">
-                <h3>ABOUT MYSELF</h3>
+                <h3>ABOUT <br />MYSELF</h3>
                 <p>
                   I am <b>Gokul Krishna</b>, a <b>cybersecurity specialist</b>,
                   adept at developing innovative solutions for intricate
@@ -26,40 +30,40 @@ function Hero() {
                   with strategic insight for enhanced <b>digital security</b>{" "}
                   and <b>seamless user experiences</b>.
                 </p>
-                <div className="skills">
-                  <ul className="skills-list">
-                    <li>
-                      <img src={assets.python} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.html} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.mysql} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.java} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.premierPro} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.photoShop} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.figma} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.unity} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.unreal} alt="" />
-                    </li>
-                    <li>
-                      <img src={assets.blender} alt="" />
-                    </li>
-                  </ul>
-                </div>
+              </div>
+              <div className="skills hero-skills">
+                <ul className="skills-list">
+                  <li>
+                    <img src={assets.python} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.html} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.mysql} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.java} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.premierPro} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.photoShop} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.figma} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.unity} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.unreal} alt="" />
+                  </li>
+                  <li>
+                    <img src={assets.blender} alt="" />
+                  </li>
+                </ul>
               </div>
             </div>
             <div className="hero-services">
