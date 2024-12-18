5b67bd5ca0ca18594387a1590a8ba3603dff65b2
Adesh-111 <adeshabinesh25@gmail.com>
2024-12-18 18:55:19 +0530

    Overflow error resolved #3

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,11 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init({
-  duration: 1500,
-  once: false, 
+  offset: 120, 
+  duration: 600,
+  easing: "ease-in-out", 
+  once: true, 
+  mirror: false,
 });
 AOS.refresh(); 
 
